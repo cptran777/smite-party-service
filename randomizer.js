1fe290b030b0c814e5578919d3361a0d98232792
cptran777 <cptran14@gmail.com>
2016-09-20 16:22:18 -0700

    set up randomizer function

--- randomizer.js
+++ randomizer.js
@@ -1,4 +1,11 @@
-module.exports = function(players, list) {
+// This function performs the random assignment for the players by role then by in each role. 
+// Players is expected to be an array list, roles is expected to be an array list, and gods
+// is expected to be an object where each property is a role and each value is an array of gods
+// suitable to that role. 
+module.exports = function(players, roles, gods) {
+  
+  
+
   return {
   	players: players,
   	list: list
