4406a64fe2d31635c5e287060e1aed44cb32fd4e
cptran777 <cptran14@gmail.com>
2016-09-22 16:54:04 -0700

    using strict since heroku doesn't allow block scope declarations outside strict mode yet

--- randomizer.js
+++ randomizer.js
@@ -1,3 +1,4 @@
+'use strict'
 // This function performs the random assignment for the players by role then by in each role. 
 // Players is expected to be an array list, roles is expected to be an array list, and gods
 // is expected to be an object where each property is a role and each value is an array of gods
